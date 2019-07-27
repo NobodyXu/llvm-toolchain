be78112e502f00faf7a05b00bfc127c75307f017
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-27 22:25:15 +1000

    Sleep 10s in git wrapper when failed in run_install_clang.sh si that the server can cool down

--- run_install_clang.sh
+++ run_install_clang.sh
@@ -6,6 +6,7 @@ git() {
     while true; do
         /usr/bin/env GIT_HTTP_LOW_SPEED_LIMIT=-0.1 GIT_HTTP_LOW_SPEED_TIME=999999 git "$@"
         [ $? -ne 0 ] && [ -n "$1" ] && ([ "$1" = "clone" ] || [ "$1" = "fetch" ] || [ "$1" = "pull" ]) || break
+	sleep 10s
     done
 }
 
