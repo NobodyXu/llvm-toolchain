70f4190e24ee9fb2ab9caaf9b19d112956eb50e0
Nobody XU <nobodyxu@lolol.me>
2019-08-15 14:23:47 +1000

    Add missing build_base.sh for Makefile

---
+++ build_base.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo $([ -n "${APT_PROXY_HOST}" ] && echo "--build-arg=APT_PROXY_HOST=${APT_PROXY_HOST}") \
+
+exec docker build base --tag="nobodyxu/llvm-toolchain:base" \
+	               $(${NO_APT_PROXY} && echo "--build-arg APT_PROXY_PORT=''") \
+	               $([ -n "${APT_PROXY_HOST}" ] && echo "--build-arg=APT_PROXY_HOST=${APT_PROXY_HOST}") \
+	               $([ -n "${GIT_PROXY_HOST}" ] && echo "--build-arg=GIT_PROXY_HOST=${GIT_PROXY_HOST}") \
+		       $(${NO_GIT_PROXY} && echo "--build-arg GIT_PROXY_PORT=''")
