d87d863d75276dcda28e085c852d9181fc8e9b99
NobodyXu <nobodyxu@lolol.me>
2019-08-17 09:34:21 +1000

    Use another way of trigger specific tag of docker hub automated build by pushing to different branch in the `post_push` hook.

---
+++ run_dependents.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+git clone https://github.com/NobodyXu/llvm-toolchain.git
+git checkout ${1}
+git merge -ff-only master
+
+# Set up user, email and passwd
+git config --local user.name "$GITHUB_ROBOT_USER"
+git config --local user.email "$GITHUB_ROBOT_EMAIL"
+
+echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
+
+exec git push
