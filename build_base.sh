54f893c786f5fbe841f6e2b9b789026405ae9938
Nobody XU <nobodyxu@lolol.me>
2019-08-16 20:26:00 +1000

    Enable specifing a different port for proxy by overriding environment variables when invoking 'make'

--- build_base.sh
+++ build_base.sh
@@ -3,5 +3,7 @@
 exec docker build base --tag="nobodyxu/llvm-toolchain:base" \
 	               $(${NO_APT_PROXY} && echo "--build-arg APT_PROXY_PORT=''") \
 	               $([ -n "${APT_PROXY_HOST}" ] && echo "--build-arg=APT_PROXY_HOST=${APT_PROXY_HOST}") \
+	               $([ -n "${APT_PROXY_PORT}" ] && echo "--build-arg=APT_PROXY_PORT=${APT_PROXY_PORT}") \
+	               $([ -n "${GIT_PROXY_PORT}" ] && echo "--build-arg=GIT_PROXY_PORT=${GIT_PROXY_PORT}") \
 	               $([ -n "${GIT_PROXY_HOST}" ] && echo "--build-arg=GIT_PROXY_HOST=${GIT_PROXY_HOST}") \
 		       $(${NO_GIT_PROXY} && echo "--build-arg GIT_PROXY_PORT=''")
