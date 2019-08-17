461b2f7625630d8f7cbaa487c4a706f9786fe953
NobodyXu <nobodyxu@lolol.me>
2019-08-17 10:14:18 +1000

    Fixd chain build for docker hub autobuild

--- run_dependents.sh
+++ run_dependents.sh
@@ -8,6 +8,7 @@ git merge -ff-only master
 git config --local user.name "$GITHUB_ROBOT_USER"
 git config --local user.email "$GITHUB_ROBOT_EMAIL"
 
+git config --local credential.helper store
 echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
 
 exec git push
