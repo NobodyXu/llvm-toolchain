b25cdcb2143255a7f6105eabdfb0cb89590eb759
NobodyXu <nobodyxu@lolol.me>
2019-08-17 12:24:44 +1000

    Fix git push error about push.default not configured

--- base/hooks/run_dependents.sh
+++ base/hooks/run_dependents.sh
@@ -7,6 +7,7 @@ git merge --ff-only master
 # Set up user, email and passwd
 git config --local user.name "$GITHUB_ROBOT_USER"
 git config --local user.email "$GITHUB_ROBOT_EMAIL"
+git config --global push.default simple
 
 git config --local credential.helper store
 echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
