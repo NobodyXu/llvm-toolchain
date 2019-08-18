90d8052c120bab70fac1e80dc9415cbc14620b2a
NobodyXu <nobodyxu@lolol.me>
2019-08-18 12:00:17 +1000

    In base/detect-*.sh, evaluate HOST_IP only when needed

--- base/detect-apt-proxy.sh
+++ base/detect-apt-proxy.sh
@@ -3,12 +3,13 @@
 # https://github.com/sameersbn/docker-apt-cacher-ng
 # https://gist.github.com/dergachev/8441335
 
-HOST_IP=$(awk '/^[a-z]+[0-9]+\t00000000/ { printf("%d.%d.%d.%d\n", "0x" substr($3, 7, 2), "0x" substr($3, 5, 2), "0x" substr($3, 3, 2), "0x" substr($3, 1, 2)) }' < /proc/net/route)
+HOST_IP=
 
 APT_PROXY_HOST=$1
 APT_PROXY_PORT=$2
 
-[ -z "$APT_PROXY_HOST" ] && APT_PROXY_HOST="$HOST_IP"
+# Use host IP if not specified
+[ -z "$APT_PROXY_HOST" ] && APT_PROXY_HOST=$(awk '/^[a-z]+[0-9]+\t00000000/ { printf("%d.%d.%d.%d\n", "0x" substr($3, 7, 2), "0x" substr($3, 5, 2), "0x" substr($3, 3, 2), "0x" substr($3, 1, 2)) }' < /proc/net/route)
 [ -z "$APT_PROXY_PORT" ] && APT_PROXY_PORT="8000"
 
 # The third condition testing command automatically test whether ${APT_PROXY_PORT} on ${HOST_IP} is open.
--- base/detect-git-proxy.sh
+++ base/detect-git-proxy.sh
@@ -3,14 +3,15 @@
 # https://github.com/sameersbn/docker-apt-cacher-ng
 # https://gist.github.com/dergachev/8441335
 
-HOST_IP=$(awk '/^[a-z]+[0-9]+\t00000000/ { printf("%d.%d.%d.%d\n", "0x" substr($3, 7, 2), "0x" substr($3, 5, 2), "0x" substr($3, 3, 2), "0x" substr($3, 1, 2)) }' < /proc/net/route)
+HOST_IP=
 
 # The following code add git proxy
 # It is writen by nobodyxu
 GIT_PROXY_HOST="$3"
 GIT_PROXY_PORT="$4"
 
-[ -z "$GIT_PROXY_HOST" ] && GIT_PROXY_HOST="$HOST_IP"
+# Use host IP if not specified
+[ -z "$GIT_PROXY_HOST" ] && GIT_PROXY_HOST=$(awk '/^[a-z]+[0-9]+\t00000000/ { printf("%d.%d.%d.%d\n", "0x" substr($3, 7, 2), "0x" substr($3, 5, 2), "0x" substr($3, 3, 2), "0x" substr($3, 1, 2)) }' < /proc/net/route)
 [ -z "$GIT_PROXY_PORT" ] && GIT_PROXY_PORT="8080"
 
 if [[ ! -z "$GIT_PROXY_PORT" ]] && [[ ! -z "$GIT_PROXY_HOST" ]] && (echo >"/dev/tcp/${GIT_PROXY_HOST}/${GIT_PROXY_PORT}") &>/dev/null; then
