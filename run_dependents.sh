38876daedfac3ed1fac64acb48091e496d64e3f8
NobodyXu <nobodyxu@lolol.me>
2019-08-23 22:03:46 +1000

    In post_push hook, only push to github when the merge actually do something. This is a patch tring to fix the problem that the build tasks in docker hub always increases.

--- run_dependents.sh
+++ run_dependents.sh
@@ -12,4 +12,6 @@ git config --local push.default simple
 git config --local credential.helper store
 echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
 
-exec git push origin HEAD
+if ! git diff --quiet master ${1}; then
+    exec git push origin HEAD
+fi
