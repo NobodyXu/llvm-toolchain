70c449c694f266904e4fc8daed3cf669bee68dfe
NobodyXu <nobodyxu@lolol.me>
2019-08-24 10:12:11 +1000

    Fix commit 38876daedfac3ed1fac64acb48091e496d64e3f8

--- run_dependents.sh
+++ run_dependents.sh
@@ -3,15 +3,16 @@
 git clone https://github.com/NobodyXu/llvm-toolchain.git
 cd llvm-toolchain
 git checkout ${1}
-git merge --ff-only master
-
-# Set push.default for push
-git config --local push.default simple
-
-# Set up username and passwd
-git config --local credential.helper store
-echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
 
 if ! git diff --quiet master ${1}; then
+    git merge --ff-only master
+    
+    # Set push.default for push
+    git config --local push.default simple
+    
+    # Set up username and passwd
+    git config --local credential.helper store
+    echo "https://${GITHUB_ROBOT_USER}:${GITHUB_ROBOT_ACCESS_TOKEN}@github.com" > ~/.git-credentials
+    
     exec git push origin HEAD
 fi
